2d6e0014870f5418df8da7f0726427609bca436f
AlbertoDiezBosonit <90448446+AlbertoDiezBosonit@users.noreply.github.com>
2022-01-07 11:36:59 +0100

    Primer commit

---
+++ src/models/Estudiante_asignatura.js
@@ -0,0 +1,41 @@
+import { Sequelize} from "sequelize";
+import { sequelize } from "../database/database"; // importamos la cadena de conexion
+import Estudiante from "./Estudiante";
+
+const Estudiante_asignatura = sequelize.define('estudiante_asignatura',{
+    id_asignatura:{
+        type: Sequelize.INTEGER,  // este se ha quedado en integer
+        primaryKey:true,
+        //defaultValue: Sequelize.UUIDV4,
+        autoIncrement: true
+    },  
+    asignatura:{
+        type: Sequelize.TEXT
+    },  
+    coments: {
+        type: Sequelize.TEXT
+    },
+    finish_date:  {
+        type: Sequelize.DATE
+    },
+    finish_date:  {
+        type: Sequelize.DATE
+    },
+    id_estudiante: {
+        type: Sequelize.UUIDV4,
+        references: {
+            model: Estudiante, //'estudiante',
+            key: 'id',
+        },
+    },
+    
+},{
+    timestamps:false
+})
+
+Estudiante.hasMany(Estudiante_asignatura,{
+    foreignKey:'id_estudiante'
+});
+Estudiante_asignatura.belongsTo(Estudiante);
+
+export default Estudiante_asignatura;
